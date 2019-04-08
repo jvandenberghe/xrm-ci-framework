3bb514d287e7b431f17b40f79c00af8e8b17cbb9
Sean Phillips <sphillips@firstkeyhomes.com>
2019-04-08 10:45:58 -0400

    Added file.

---
+++ CRM365/Xrm.Framework.CI.Sample/ExportSolutionSample.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+
+param
+(
+	[string]$connectionString = "URL=https://org5b2169f3.crm.dynamics.com; UserName=sphillips@firstkeyhomes.com; Password=Pokemonprs1221!; AuthType=Office365" #The connection string as per CRM Sdk
+)
+
+$ErrorActionPreference = "Stop"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$VerbosePreference = "Continue"
+
+Write-Verbose "Script Path: $scriptPath"
+Write-Verbose "ConnectionString = $connectionString"
+
+& "$scriptPath\Lib\xRMCIFramework\ExportSolution.ps1" -Verbose -CrmConnectionString $connectionString -SolutionName "Announcements" -ExportSolutionOutputPath "C:\Users\sphillips\Desktop\Output"
